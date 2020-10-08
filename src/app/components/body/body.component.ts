d17048e1df02959f1a034fd9a7d19effe71ab555
Emil Mikulski <emilmiulski0@gamil.com>
2020-10-08 18:21:20 +0200

    create body box

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -23,8 +23,8 @@ export class BodyComponent implements OnInit {
     new Vehicle(2, 'C FORCE 550 EFI EPS', 28900, 'assets/images/c-force-550-efi-eps.jpg'),
     new Vehicle(3, 'C FORCE 820 EFI EPS', 39500, 'assets/images/c-force-800-efi-eps.jpg'),
     new Vehicle(4, 'C FORCE 850 EFI EPS', 44500, 'assets/images/c-force-850-efi-eps.jpg'),
-    new Vehicle(5, 'C FORCE 1000 (CC)', 49900, 'assets/images/c-force-800-efi-eps.jpg')    
-  ]
+    new Vehicle(5, 'C FORCE 1000 (CC)', 49900, 'assets/images/c-force-800-efi-eps.jpg')
+  ];
 
   constructor() { }
 
