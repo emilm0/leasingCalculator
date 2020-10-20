1ec9e9a255091d431a05d85915e60a73b260ec78
Emil Mikulski <emilmiulski0@gamil.com>
2020-10-20 22:41:20 +0200

    add buttons

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -18,6 +18,8 @@ export class Vehicle {
 })
 export class BodyComponent implements OnInit {
 
+  counter = 0;
+
   vehicles = [
     new Vehicle(1, 'C FORCE 520 EFI', 23900, 'assets/images/c-force-520-efi.jpg'),
     new Vehicle(2, 'C FORCE 550 EFI EPS', 28900, 'assets/images/c-force-550-efi-eps.jpg'),
@@ -25,6 +27,27 @@ export class BodyComponent implements OnInit {
     new Vehicle(4, 'C FORCE 850 EFI EPS', 44500, 'assets/images/c-force-850-efi-eps.jpg'),
     new Vehicle(5, 'C FORCE 1000 (CC)', 49900, 'assets/images/c-force-800-efi-eps.jpg')
   ];
+
+  checkItem(i: number): number{
+    if (i > this.vehicles.length){
+      return 0;
+    }
+    else if (i < 0){
+      return this.vehicles.length - 1;
+    }
+    else{
+      return i - 1;
+    }
+  }
+
+  next(): number{
+    console.log(this.counter);
+    return this.checkItem (this.counter + 1);
+  }
+  prev(): number{
+    console.log(this.counter);
+    return this.checkItem (this.counter - 1);
+  }
 
   constructor() { }
 
