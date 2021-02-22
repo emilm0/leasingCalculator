b35f75ae1eb8dc69a2ccce2a13ee5aa4253423f0
Emil Mikulski <emilmiulski0@gamil.com>
2021-02-22 22:28:32 +0100

    user click disabled

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state,transition, animate, style } from '@angular/animations'
+import { trigger, state,transition, animate, style, query, stagger } from '@angular/animations'
  import { VEHICLES } from 'src/app/classes/vehiclesList';
 
 @Component({
@@ -7,30 +7,17 @@ import { trigger, state,transition, animate, style } from '@angular/animations'
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css'],
   animations: [
-
-    trigger('slideInOut', [
-      state('enter', style({
-        height: '0',
-        overflow: 'hidden',
-        opacity: '0',
-        visibility: 'hidden'
-      })),
-      state('leave', style({
-        overflow: 'hidden'
-      })),
-      transition('enter<=>leave', animate(500))
-    ])
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('.hero, form', [
+          style({opacity: 0, transform: 'translateY(-100px)'}),
+          stagger(-30, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ])
+    ]),
   ]
-  //   trigger('slideInOut', [
-  //     transition(':enter', [
-  //       style({transform: 'translateY(-100%)'}),
-  //       animate('200ms ease-in', style({transform: 'translateY(0%)'}))
-  //     ]),
-  //     transition(':leave', [
-  //       animate('200ms ease-in', style({transform: 'translateY(-100%)'}))
-  //     ])
-  //   ])
-  // ]
 })
 export class BodyComponent implements OnInit {
 
