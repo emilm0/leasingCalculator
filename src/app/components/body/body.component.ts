81334585337e4d4621c69cc73e8f7d6281203d89
Emil Mikulski <emilmiulski0@gamil.com>
2021-03-12 18:45:58 +0100

    last fix

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,30 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state,transition, animate, style, query, stagger } from '@angular/animations'
+import { trigger, state,transition, animate, style, query, stagger, animation, useAnimation } from '@angular/animations'
  import { VEHICLES } from 'src/app/classes/vehiclesList';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css'],
-  animations: [
-    trigger('pageAnimations', [
-      transition(':enter', [
-        query('.hero, form', [
-          style({opacity: 0, transform: 'translateY(-100px)'}),
-          stagger(-30, [
-            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
-          ])
-        ])
-      ])
-    ]),
-  ]
+  styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
 
   counter = 0;
 
   vehicles = VEHICLES;
-  
+
   checkItem(i: number): number{
     if (i >= this.vehicles.length){
       return 0;
